275ca41b6adb5b16d17526afeccb972be6afcb6c
Dylan <dylan.sharon@ontariotechu.net>
2023-02-02 08:52:52 -0500

    Lab2 (Movement/Rotation/Jump)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,14 +5,50 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public float moveSpeed;
+
+    private Rigidbody rigidBody;
+
+    public float jumpForce;
+
+    void Awake()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Jump();
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            CheckJumpForce();
+        }
+    }
+
+    void Move()
+    {
+        //Get our inputs
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(xInput, 0, zInput) * moveSpeed;
+        dir.y = rigidBody.velocity.y;
+        rigidBody.velocity = dir;
+
+        Vector3 facingDir = new Vector3(xInput, 0, zInput);
+
+        if (facingDir.magnitude > 0)
+        {
+            transform.forward = facingDir;
+        }
+    }
+
+    void CheckJumpForce()
+    {
+        rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
