09f47fa09bea0948354f980bd33442ae2ee53ff6
Dylan10101023 <dylan.sharon@ontariotechu.net>
2023-03-27 14:23:30 -0400

    Lab9

--- Assets/Scripts/SaveLoadManager.cs
+++ Assets/Scripts/SaveLoadManager.cs
@@ -5,12 +5,24 @@ using System.IO;
 
 public class SaveLoadManager : MonoBehaviour
 {
-    private string saveFileName = "savedata.json";
+    private string saveFileName = "savedata.dat";
+
+    //Key for encryption or decryption
+    private byte[] encryptionKey = new byte[] { 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF };
 
     public void SaveGame(GameData gameData)
     {
         string json = JsonUtility.ToJson(gameData);
-        File.WriteAllText(GetSaveFilePath(), json);
+        byte[] dataBytes = System.Text.Encoding.UTF8.GetBytes(json);
+
+        // Encrypt the data
+        for (int i = 0; i < dataBytes.Length; i++)
+        {
+            dataBytes[i] ^= encryptionKey[i % encryptionKey.Length];
+        }
+
+        string encryptedData = System.Convert.ToBase64String(dataBytes);
+        File.WriteAllText(GetSaveFilePath(), encryptedData);
     }
 
     public GameData LoadGame()
@@ -18,7 +30,16 @@ public class SaveLoadManager : MonoBehaviour
         string filePath = GetSaveFilePath();
         if (File.Exists(filePath))
         {
-            string json = File.ReadAllText(filePath);
+            string encryptedData = File.ReadAllText(filePath);
+            byte[] dataBytes = System.Convert.FromBase64String(encryptedData);
+
+            // Decrypt the data
+            for (int i = 0; i < dataBytes.Length; i++)
+            {
+                dataBytes[i] ^= encryptionKey[i % encryptionKey.Length];
+            }
+
+            string json = System.Text.Encoding.UTF8.GetString(dataBytes);
             return JsonUtility.FromJson<GameData>(json);
         }
         else
